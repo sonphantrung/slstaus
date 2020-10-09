a25005563bab84cb44b772d39bc43daaed1e530e
sonphantrung <phantrungson17@gmail.com>
2020-10-09 14:05:23 +0700

    Edit

--- config.h
+++ config.h
@@ -63,11 +63,10 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ run_command, "%s|", "curl wttr.in/hanoi?format=1" },
 	{ kernel_release, " %s|", NULL },
 	{ netspeed_rx, "ﯱ %sB/s|", "wlp2s0" },
-	{ cpu_perc, "CPU:%s%%|", NULL	      },
-	{ ram_perc, "RAM:%s%%|", NULL	      },
-	{ run_command, "%s|", "/home/phantrungson/.local/bin/vol.sh" },
+	{ cpu_perc, "C:%s%%|", NULL	      },
+	{ ram_perc, "R:%s%%|", NULL	      },
+	{ run_command, "%s|", "~/.local/bin/vol.sh" },
 	{ datetime, " %s",           "%H:%M" },
 };
